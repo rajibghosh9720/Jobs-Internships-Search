4a3f002ff4790d1da1f2df0a3bbff425a902c6bd
Rajib Ghosh <78555404+rajibghosh9720@users.noreply.github.com>
2023-09-05 18:56:31 +0530

    Update Navbar.js

--- src/app/pages/Navbar.js
+++ src/app/pages/Navbar.js
@@ -1,31 +1,30 @@
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import '@fortawesome/fontawesome-svg-core/styles.css';
+import "@fortawesome/fontawesome-svg-core/styles.css";
 import { faTelegram, faFacebook, faTwitter } from "@fortawesome/free-brands-svg-icons";
-
 
 const Navbar = () => {
   return (
-    <nav className="bg-blue-100 p-4 flex justify-between items-center">
-      {/* Left Side - Company Logo */}
-      <div className="flex items-center">
+    <nav className="bg-blue-100 p-2 sm:p-4 flex flex-col sm:flex-row justify-center sm:justify-between items-center">
+      {/* Company Logo */}
+      <div className="flex items-center mb-2 sm:mb-0">
         <Image
-          src="/logo.png" 
+          src="/logo.png"
           alt="Company Logo"
-          width={200} 
-          height={50} 
+          width={200}
+          height={50}
         />
       </div>
 
-      {/* Right Side - Social Media Icons */}
+      {/* Social Media Icons */}
       <div className="flex items-center space-x-4">
-        <a href="https://t.me/entryleveljobs" className=" hover:text-blue-400">
+        <a href="https://t.me/entryleveljobs" className="hover:text-blue-400">
           <FontAwesomeIcon icon={faTelegram} size="2x" />
         </a>
-        <a href="https://www.facebook.com/groups/SummerInternshipUSA/" className=" hover:text-blue-400">
+        <a href="https://www.facebook.com/groups/SummerInternshipUSA/" className="hover:text-blue-400">
           <FontAwesomeIcon icon={faFacebook} size="2x" />
         </a>
-        <a href="https://twitter.com/entrylevel_job" className=" hover:text-blue-400">
+        <a href="https://twitter.com/entrylevel_job" className="hover:text-blue-400">
           <FontAwesomeIcon icon={faTwitter} size="2x" />
         </a>
       </div>
